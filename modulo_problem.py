5c477b04099fdb57829e7bd553a5b492aa3d2edd
Ancentus <mancentus7@gmail.com>
2023-02-07 15:36:43 +0300

    Solved

--- modulo_problem.py
+++ modulo_problem.py
@@ -1,2 +1,8 @@
 # Suppose today is Saturday and you and your friends are going to meet in 10 days.
 # What day is in 10 days?
+
+# SOLUTION
+# Saturday is the 7th day of the week
+
+solution = (7 + 10) % 7
+print("day", solution, "of the week")
